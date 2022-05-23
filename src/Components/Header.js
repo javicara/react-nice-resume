d2e40ce40fd128c18aaa676aee7923990e7723c1
javicara <javicaraballo1@gmail.com>
2022-05-23 19:05:27 -0300

    commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,23 +30,6 @@ class Header extends Component {
               </a>
             </li>
 
-            <li>
-              <a className="smoothscroll" href="#about">
-                About
-              </a>
-            </li>
-
-            <li>
-              <a className="smoothscroll" href="#resume">
-                Resume
-              </a>
-            </li>
-
-            <li>
-              <a className="smoothscroll" href="#portfolio">
-                Works
-              </a>
-            </li>
 
             <li>
               <a className="smoothscroll" href="#contact">
